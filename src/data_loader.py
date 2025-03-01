37601d09b959f9876b1a747f4f53734911e9d14c
Yasith Gunawardhana <gunawardhanayasith@gmail.com>
2025-03-01 18:36:33 +0530

    Fix: error in data data_loader

--- src/data_loader.py
+++ src/data_loader.py
@@ -6,11 +6,11 @@ class DataLoader:
     def __init__(self, file_path):
         self.file_path = file_path
 
-        def load_data(self):
-            try:
-                data = pd.read_csv(self.file_path)
-                print("Data loaded successfully")
-                return data
-            except Exception as e:
-                print(f"Error loding data: {e}")
+    def load_data(self):
+        try:
+            data = pd.read_csv(self.file_path)
+            print("Data loaded successfully")
+            return data
+        except Exception as e:
+            print(f"Error loding data: {e}")
 
